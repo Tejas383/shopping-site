a0446bba49dd28c4f21ba43e5fbd79d54113d87f
Tejasvita <ttejasvita@gmail.com>
2026-01-23 20:16:53 +0530

    feat: implement pagination for product cards in Cards component

--- src/components/Cards.jsx
+++ src/components/Cards.jsx
@@ -1,17 +1,75 @@
-import React from 'react'
-import productsData from '../data/products.json'
-import CardComponent from './CardComponent.jsx'
+import React, { useState } from "react";
+import productsData from "../data/products.json";
+import CardComponent from "./CardComponent.jsx";
+import {
+  Pagination,
+  PaginationContent,
+  PaginationItem,
+  PaginationLink,
+  PaginationNext,
+  PaginationPrevious,
+} from "@/components/ui/pagination";
 
-const Cards = ({cols}) => {
-  console.log(cols)
+const Cards = ({ cols }) => {
+  const [currentPage, setCurrentPage] = useState(1);
+
+  const totalItems = productsData.length;
+  const itemsPerPage = 10;
+
+  const numberOfPages = Math.ceil(totalItems / itemsPerPage);
+
+  const startIndex = (currentPage - 1) * itemsPerPage;
+  const endIndex = startIndex + itemsPerPage;
+
+  const visibleItems = productsData.slice(startIndex, endIndex);
+
+  // console.log(cols);
+
   return (
-    <div className="grid  p-3 gap-3 w-full" style={{ gridTemplateColumns: `repeat(${cols}, minmax(0, 1fr))` }}>
-    {/* <div className={`grid grid-cols-[${cols}] gap-3 p-3 w-full`}> */}
-      {productsData.map((item, index) => (
-        <CardComponent key={index} item={item} />
-      ))}
-    </div>
-  )
-}
+    <div className="w-full">
+      {/* Pagination */}
+      <Pagination className="mt-3">
+        <PaginationContent className="">
+          <PaginationItem>
+            <PaginationPrevious
+              onClick={() => setCurrentPage((p) => Math.max(p - 1, 1))}
+              className="hover:bg-gray-200 hover:border-gray-400 hover:border"
+            />
+          </PaginationItem>
 
-export default Cards
+          {Array.from({ length: numberOfPages }).map((_, i) => (
+            <PaginationItem key={i}>
+              <PaginationLink
+                isActive={currentPage === i + 1}
+                onClick={() => setCurrentPage(i + 1)}
+                className="hover:bg-gray-200 hover:border-gray-400 hover:border"
+              >
+                {i + 1}
+              </PaginationLink>
+            </PaginationItem>
+          ))}
+
+          <PaginationItem>
+            <PaginationNext
+              onClick={() =>
+                setCurrentPage((p) => Math.min(p + 1, numberOfPages))
+              }
+              className="hover:bg-gray-200 hover:border-gray-400 hover:border"
+            />
+          </PaginationItem>
+        </PaginationContent>
+      </Pagination>
+
+      <div
+        className="grid  p-3 gap-3 w-full"
+        style={{ gridTemplateColumns: `repeat(${cols}, minmax(0, 1fr))` }}
+      >
+        {visibleItems.map((item, index) => (
+          <CardComponent key={index} item={item} />
+        ))}
+      </div>
+    </div>
+  );
+};
+
+export default Cards;
